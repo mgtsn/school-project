e6b828cab9a86ca23bd647ee82f3ae0e49eac5f0
m <morgantosini@gmail.com>
2019-01-30 09:13:30 -0500

    Menu UI

---
+++ Game Lab Project/Assets/Standard Assets/2D/Scripts/LoadScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+
+    public void LoadFileSelect()
+    {
+        SceneManager.LoadScene("File Select");
+    }
+
+    public void Load(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
