654933539eb96eb3dd1fc9a781698d9668444cf1
m <morgantosini@gmail.com>
2019-02-06 10:09:27 -0500

    Working save

--- Game Lab Project/Assets/Standard Assets/2D/Scripts/CheckpointManager.cs
+++ Game Lab Project/Assets/Standard Assets/2D/Scripts/CheckpointManager.cs
@@ -6,18 +6,53 @@ public class CheckpointManager : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public static SaveGame s;
 
     public Vector3 currentCheckpoint;
+
+    public Save save;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Checkpoint")
         {
             currentCheckpoint = collision.transform.position;
-            s.Save();
+            Save();
         }
     }
+
+
+
+    public void Save()
+    {
+
+        save = new Save();
+        save.posx = currentCheckpoint.x;
+        save.posy = currentCheckpoint.y;
+        save.posz = currentCheckpoint.z;
+
+
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savefile1.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savefile1.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savefile1.save", FileMode.Open);
+            Save s = (Save)bf.Deserialize(file);
+            file.Close();
+
+            currentCheckpoint = new Vector3(s.posx, s.posy, s.posz);
+            Respawn();
+        }
+    }
+
 
 
     public void Respawn()
@@ -37,6 +72,9 @@ public class CheckpointManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Load"))
+        {
+            Load();
+        }
     }
 }
